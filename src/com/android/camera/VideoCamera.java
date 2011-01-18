8d12f1fc53b2904f6037fb1916637da2d46dd488
Thomas Hultgren <thomas.hultgren.x@sonyericsson.com>
2011-01-18 10:56:25 -0800

    Camcorder HUD no longer disappears when opening slider
    The camcorder head up display was incorrectly detached when opening
    the slider. Changed behavior to conform with the camera head up
    display.
    
    Change-Id: Ie52ab641c0702cbae5e04530a40f992ded8923bc

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -405,8 +405,8 @@ public class VideoCamera extends BaseCamera
         // becomes landscape.
         Configuration config = getResources().getConfiguration();
         if (config.orientation == Configuration.ORIENTATION_LANDSCAPE
-                && !mPausing && mGLRootView == null) {
-            attachHeadUpDisplay();
+                && !mPausing) {
+            if (mGLRootView == null) attachHeadUpDisplay();
         } else if (mGLRootView != null) {
             detachHeadUpDisplay();
         }
